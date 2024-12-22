f8a185138826b97ca511187bc48a53264d107eb6
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-23 00:31:10 +0530

    clear unused line of code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 import express from "express";
-import createHttpError from "http-errors";
 import globalErrorHandler from "../middleware/globalErrorHandler.js";
 
 const app = express();
 
 // routes
 app.get("/", (req, res) => {
-  const error = createHttpError(400, "Somthing went wrong");
-  throw error;
-  //   res.status(200).json({
-  //     message: "successfully connected to our site",
-  //     success: true,
-  //     error: false,
-  //   });
+  res.status(200).json({
+    message: "successfully connected to our site",
+    success: true,
+    error: false,
+  });
 });
 
 // global error handler
