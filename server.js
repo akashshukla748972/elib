e963c1eaa6fd9af0cd7c90184032ebfadf66f8de
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-22 22:59:36 +0530

    add config

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+import { config } from "./config/config.js";
 import app from "./src/app.js";
 
 const startServer = () => {
-  const PORT = process.env.PORT || 3000;
+  const PORT = config.port || 3000;
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
