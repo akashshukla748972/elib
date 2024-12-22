1856b30464a955931f527c675d6aa488297673a1
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-23 00:26:49 +0530

    add global error handler

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGODB_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "../middleware/globalErrorHandler.js";
 
 const app = express();
 
 // routes
 app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "successfully connected to our site",
-    success: true,
-    error: false,
-  });
+  const error = createHttpError(400, "Somthing went wrong");
+  throw error;
+  //   res.status(200).json({
+  //     message: "successfully connected to our site",
+  //     success: true,
+  //     error: false,
+  //   });
 });
+
+// global error handler
+app.use(globalErrorHandler);
 
 export default app;
