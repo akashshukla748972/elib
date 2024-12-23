b4981fd533033fdb2a6b260c6511d4c43d388211
akashshukla748972 <shuklaakash748972@gmail.com>
2024-12-23 22:20:27 +0530

    add image or file url in database

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,44 @@
+import createHttpError from "http-errors";
 import uploadImageCloudinary from "../config/cloudinary.js";
+import Book from "../models/bookModel.js";
 
 const createBook = async (req, res, next) => {
-  console.log("file : ", req.files);
-  const uploadResult = await uploadImageCloudinary(req.files);
-  console.log("Upload result : ", uploadResult);
-  return res.json({ message: "Ok" });
+  try {
+    const { title, genre } = req.body;
+    const { cover_image, file } = req.files;
+
+    // Validation
+    if (!cover_image || !file || !title || !genre) {
+      const errorFields = [];
+      if (!cover_image) errorFields.push("Cover Image");
+      if (!file) errorFields.push("File");
+      if (!title) errorFields.push("Title");
+      if (!genre) errorFields.push("Genre");
+      const errorMessage = `${errorFields.join(", ")} is required`;
+      return next(createHttpError(400, errorMessage));
+    }
+
+    // upload image in cloudinary
+    const uploadResult = await uploadImageCloudinary(req.files);
+    console.log("upload image", uploadResult);
+    // Process to create book
+    const newBook = await Book.create({
+      title,
+      genre,
+      author: "67687a0a78857d088c8486ae",
+      cover_image: uploadResult.uploadImage.url,
+      file: uploadResult.uploadPdf.url,
+    });
+
+    return res.status(201).json({
+      message: "Ok",
+      cover_image: newBook.cover_image,
+      file: newBook.file,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    createHttpError(500, "Internal server error");
+  }
 };
 
 export { createBook };
