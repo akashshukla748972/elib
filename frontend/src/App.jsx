cadd91d222774a8aa7056f1ada83f049c02abd3b
akashshukla748972 <shuklaakash748972@gmail.com>
2025-02-28 00:57:35 +0530

    add home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import MainRouting from "./utils/MainRouting";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl font-semibold">Elib Home Page</h1>
+    <div
+      className={`min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-b from-gray-50 via-blue-50 to-gray-100`}
+      r
+    >
+      <MainRouting />
     </div>
   );
 };
